76d7415c39993e76d67d8f20a66c0082013e706f
dhi1ka <daw.knight04@gmail.com>
2022-06-05 11:35:17 +0700

    product detail pages container

--- client/src/pages/ProductDetail/ProductDetail.jsx
+++ client/src/pages/ProductDetail/ProductDetail.jsx
@@ -1,13 +1,39 @@
 import React from "react";
 import Footer from "../../components/Footer/Footer";
 import Navbar from "../../components/Navbar/Navbar";
-import { Container } from "./ProductDetailStyles";
+import Announcement from "../../components/Announcement/Announcement";
+import Newsletter from "../../components/Newsletter/Newsletter";
+import {
+  Container,
+  Desc,
+  Image,
+  ImgContainer,
+  InfoContainer,
+  Price,
+  Title,
+  Wrapper,
+} from "./ProductDetailStyles";
 
 const ProductDetail = () => {
   return (
     <Container>
       <Navbar />
-      <h1>Product Detail</h1>
+      <Announcement />
+      <Wrapper>
+        <ImgContainer>
+          <Image />
+        </ImgContainer>
+        <InfoContainer>
+          <Title>Product Detail</Title>
+          <Desc>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Nihil
+            velit vel neque minus similique obcaecati suscipit tempore
+            laboriosam molestias dolore.
+          </Desc>
+          <Price>20000</Price>
+        </InfoContainer>
+      </Wrapper>
+      <Newsletter />
       <Footer />
     </Container>
   );
